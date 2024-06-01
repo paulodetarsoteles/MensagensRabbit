64654571383ee76c643fda130af4f47bb5d71e2d
Paulo de Tarso <potassio@hotmail.com>
2024-06-01 17:11:25 -0300

    Implementação inicial da Api que recebe as mensagens do frontend

---
+++ backend/Mensagens/Mensagens.API/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mensagens.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Ok("API funcionando!");
+    }
+}
